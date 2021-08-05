f42f2bc203e9fcecdbafd0e9411b955736d9c1c2
vitorvenacio <vitorvenacio@gmail.com>
2021-08-05 17:59:00 -0300

    alterações de hoje

---
+++ src/app/conversor/components/conversor.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Moeda, Conversao, ConversaoResponse } from '../models';
+import { MoedaService, ConversorService } from '../services';
+
+@Component({
+  selector: 'app-conversor',
+  templateUrl: './conversor.component.html',
+  styleUrls: ['./conversor.component.css']
+})
+export class ConversorComponent implements OnInit {
+
+  moedas: Moeda[];
+  conversao: Conversao;
+  possuiErro: boolean;
+  conversaoResponse: ConversaoResponse;
+
+
+  @ViewChild("conversaoForm", { static: true }) conversaoForm: NgForm;
+  constructor(  
+    private moedaService: MoedaService,
+    private conversorService: ConversorService) { }
+    
+  ngOnInit(): void {
+    this.moedas = this.moedaService.listarTodas();
+    this.init();
+  }
+
+  init(): void {
+  	this.conversao = new Conversao('EUR', 'BRL', null);
+  	this.possuiErro = false;
+  }
+
+  converter(): void {
+    if (this.conversaoForm.form.valid) {
+      console.log('teste' + JSON.stringify(this.conversao));
+      this.conversorService
+        .converter(this.conversao)
+        .subscribe(
+          response => this.conversaoResponse = response,
+          error => this.possuiErro = true
+        );
+    }
+  }
+
+}
